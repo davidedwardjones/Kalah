29690be244eb152e6c477f22cd94f11711db5d94
isaac006 <isaac006_2046@yahoo.com.hk>
2013-11-30 21:15:47 +0000

    random agent

--- MKAgent/Kalah.java
+++ MKAgent/Kalah.java
@@ -15,9 +15,9 @@ public class Kalah
      */
     public Kalah (Board board) throws NullPointerException
     {
-    	if (board == null)
-    		throw new NullPointerException();
-    	this.board = board;
+      if (board == null)
+        throw new NullPointerException();
+      this.board = board;
     }
 
     /**
@@ -25,7 +25,7 @@ public class Kalah
      */
     public Board getBoard ()
     {
-		return board;
+    return board;
     }
 
     /**
@@ -36,7 +36,7 @@ public class Kalah
      */
     public boolean isLegalMove (Move move)
     {
-    	return isLegalMove(board, move);
+      return isLegalMove(board, move);
     }
 
     /**
@@ -55,7 +55,7 @@ public class Kalah
      */
     public Side makeMove (Move move)
     {
-    	return makeMove(board, move);
+      return makeMove(board, move);
     }
 
     /**
@@ -64,7 +64,7 @@ public class Kalah
      */
     public boolean gameOver()
     {
-    	return gameOver(board);
+      return gameOver(board);
     }
 
     /**
@@ -76,9 +76,9 @@ public class Kalah
      */
     public static boolean isLegalMove (Board board, Move move)
     {
-    	// check if the hole is existent and non-empty:
-    	return (move.getHole() <= board.getNoOfHoles())
-    	       && (board.getSeeds(move.getSide(), move.getHole()) != 0);
+      // check if the hole is existent and non-empty:
+      return (move.getHole() <= board.getNoOfHoles())
+             && (board.getSeeds(move.getSide(), move.getHole()) != 0);
     }
 
     /**
@@ -98,113 +98,113 @@ public class Kalah
      */
     public static Side makeMove (Board board, Move move)
     {
-		/* from the documentation:
-		  "1. The counters are lifted from this hole and sown in anti-clockwise direction, starting
-		      with the next hole. The player's own kalahah is included in the sowing, but the
-		      opponent's kalahah is skipped.
-		   2. outcome:
-		    	1. if the last counter is put into the player's kalahah, the player is allowed to
-		    	   move again (such a move is called a Kalah-move);
-		    	2. if the last counter is put in an empty hole on the player's side of the board
-		    	   and the opposite hole is non-empty,
-		    	   a capture takes place: all stones in the opposite opponents pit and the last
-		    	   stone of the sowing are put into the player's store and the turn is over;
-		    	3. if the last counter is put anywhere else, the turn is over directly.
-		   3. game end:
-		    	The game ends whenever a move leaves no counters on one player's side, in
-		    	which case the other player captures all remaining counters. The player who
-		    	collects the most counters is the winner."
-		*/
+    /* from the documentation:
+      "1. The counters are lifted from this hole and sown in anti-clockwise direction, starting
+          with the next hole. The player's own kalahah is included in the sowing, but the
+          opponent's kalahah is skipped.
+       2. outcome:
+          1. if the last counter is put into the player's kalahah, the player is allowed to
+             move again (such a move is called a Kalah-move);
+          2. if the last counter is put in an empty hole on the player's side of the board
+             and the opposite hole is non-empty,
+             a capture takes place: all stones in the opposite opponents pit and the last
+             stone of the sowing are put into the player's store and the turn is over;
+          3. if the last counter is put anywhere else, the turn is over directly.
+       3. game end:
+          The game ends whenever a move leaves no counters on one player's side, in
+          which case the other player captures all remaining counters. The player who
+          collects the most counters is the winner."
+    */
 
 
-    	// pick seeds:
-    	int seedsToSow = board.getSeeds(move.getSide(), move.getHole());
-    	board.setSeeds(move.getSide(), move.getHole(), 0);
+      // pick seeds:
+      int seedsToSow = board.getSeeds(move.getSide(), move.getHole());
+      board.setSeeds(move.getSide(), move.getHole(), 0);
 
-    	int holes = board.getNoOfHoles();
-    	int receivingPits = 2*holes + 1;  // sow into: all holes + 1 store
-    	int rounds = seedsToSow / receivingPits;  // sowing rounds
-    	int extra = seedsToSow % receivingPits;  // seeds for the last partial round
-    	/* the first "extra" number of holes get "rounds"+1 seeds, the
-    	   remaining ones get "rounds" seeds */
+      int holes = board.getNoOfHoles();
+      int receivingPits = 2*holes + 1;  // sow into: all holes + 1 store
+      int rounds = seedsToSow / receivingPits;  // sowing rounds
+      int extra = seedsToSow % receivingPits;  // seeds for the last partial round
+      /* the first "extra" number of holes get "rounds"+1 seeds, the
+         remaining ones get "rounds" seeds */
 
-    	// sow the seeds of the full rounds (if any):
-    	if (rounds != 0)
-    	{
-    		for (int hole = 1; hole <= holes; hole++)
-    		{
-        		board.addSeeds(Side.NORTH, hole, rounds);
-        		board.addSeeds(Side.SOUTH, hole, rounds);
-    		}
-    		board.addSeedsToStore(move.getSide(), rounds);
-    	}
+      // sow the seeds of the full rounds (if any):
+      if (rounds != 0)
+      {
+        for (int hole = 1; hole <= holes; hole++)
+        {
+            board.addSeeds(Side.NORTH, hole, rounds);
+            board.addSeeds(Side.SOUTH, hole, rounds);
+        }
+        board.addSeedsToStore(move.getSide(), rounds);
+      }
 
-    	// sow the extra seeds (last round):
-    	Side sowSide = move.getSide();
-    	int sowHole = move.getHole();  // 0 means store
-    	for (; extra > 0; extra--)
-    	{
-    		// go to next pit:
-    		sowHole++;
-    		if (sowHole == 1)  // last pit was a store
-    			sowSide = sowSide.opposite();
-    		if (sowHole > holes)
-    		{
-    			if (sowSide == move.getSide())
-    			{
-    				sowHole = 0;  // sow to the store now
-    	    		board.addSeedsToStore(sowSide, 1);
-    	    		continue;
-    			}
-    			else
-    			{
-        			sowSide = sowSide.opposite();
-    				sowHole = 1;
-    			}
-    		}
-    		// sow to hole:
-    		board.addSeeds(sowSide, sowHole, 1);
-    	}
+      // sow the extra seeds (last round):
+      Side sowSide = move.getSide();
+      int sowHole = move.getHole();  // 0 means store
+      for (; extra > 0; extra--)
+      {
+        // go to next pit:
+        sowHole++;
+        if (sowHole == 1)  // last pit was a store
+          sowSide = sowSide.opposite();
+        if (sowHole > holes)
+        {
+          if (sowSide == move.getSide())
+          {
+            sowHole = 0;  // sow to the store now
+              board.addSeedsToStore(sowSide, 1);
+              continue;
+          }
+          else
+          {
+              sowSide = sowSide.opposite();
+            sowHole = 1;
+          }
+        }
+        // sow to hole:
+        board.addSeeds(sowSide, sowHole, 1);
+      }
 
-    	// capture:
-    	if ( (sowSide == move.getSide())  // last seed was sown on the moving player's side ...
-    		 && (sowHole > 0)  // ... not into the store ...
-    		 && (board.getSeeds(sowSide, sowHole) == 1)  // ... but into an empty hole (so now there's 1 seed) ...
-    		 && (board.getSeedsOp(sowSide, sowHole) > 0) )  // ... and the opposite hole is non-empty
-    	{
-    		board.addSeedsToStore(move.getSide(), 1 + board.getSeedsOp(move.getSide(), sowHole));
-    		board.setSeeds(move.getSide(), sowHole, 0);
-    		board.setSeedsOp(move.getSide(), sowHole, 0);
-    	}
+      // capture:
+      if ( (sowSide == move.getSide())  // last seed was sown on the moving player's side ...
+         && (sowHole > 0)  // ... not into the store ...
+         && (board.getSeeds(sowSide, sowHole) == 1)  // ... but into an empty hole (so now there's 1 seed) ...
+         && (board.getSeedsOp(sowSide, sowHole) > 0) )  // ... and the opposite hole is non-empty
+      {
+        board.addSeedsToStore(move.getSide(), 1 + board.getSeedsOp(move.getSide(), sowHole));
+        board.setSeeds(move.getSide(), sowHole, 0);
+        board.setSeedsOp(move.getSide(), sowHole, 0);
+      }
 
-    	// game over?
-		Side finishedSide = null;
-    	if (holesEmpty(board, move.getSide()))
-    		finishedSide = move.getSide();
-    	else if (holesEmpty(board, move.getSide().opposite()))
-    		finishedSide = move.getSide().opposite();
-    		/* note: it is possible that both sides are finished, but then
-    		   there are no seeds to collect anyway */
-    	if (finishedSide != null)
-    	{
-    		// collect the remaining seeds:
-    		int seeds = 0;
-    		Side collectingSide = finishedSide.opposite();
-    		for (int hole = 1; hole <= holes; hole++)
-    		{
-    			seeds += board.getSeeds(collectingSide, hole);
-    			board.setSeeds(collectingSide, hole, 0);
-    		}
-			board.addSeedsToStore(collectingSide, seeds);
-    	}
+      // game over?
+    Side finishedSide = null;
+      if (holesEmpty(board, move.getSide()))
+        finishedSide = move.getSide();
+      else if (holesEmpty(board, move.getSide().opposite()))
+        finishedSide = move.getSide().opposite();
+        /* note: it is possible that both sides are finished, but then
+           there are no seeds to collect anyway */
+      if (finishedSide != null)
+      {
+        // collect the remaining seeds:
+        int seeds = 0;
+        Side collectingSide = finishedSide.opposite();
+        for (int hole = 1; hole <= holes; hole++)
+        {
+          seeds += board.getSeeds(collectingSide, hole);
+          board.setSeeds(collectingSide, hole, 0);
+        }
+      board.addSeedsToStore(collectingSide, seeds);
+      }
 
-    	board.notifyObservers(move);
+      board.notifyObservers(move);
 
-    	// who's turn is it?
-    	if (sowHole == 0)  // the store (implies (sowSide == move.getSide()))
-    		return move.getSide();  // move again
-    	else
-    		return move.getSide().opposite();
+      // who's turn is it?
+      if (sowHole == 0)  // the store (implies (sowSide == move.getSide()))
+        return move.getSide();  // move again
+      else
+        return move.getSide().opposite();
     }
 
     /**
@@ -215,10 +215,10 @@ public class Kalah
      */
     protected static boolean holesEmpty (Board board, Side side)
     {
-    	for (int hole = 1; hole <= board.getNoOfHoles(); hole++)
-    		if (board.getSeeds(side, hole) != 0)
-    			return false;
-		return true;
+      for (int hole = 1; hole <= board.getNoOfHoles(); hole++)
+        if (board.getSeeds(side, hole) != 0)
+          return false;
+    return true;
     }
 
     /**
@@ -228,9 +228,9 @@ public class Kalah
      */
     public static boolean gameOver (Board board)
     {
-    	// The game is over if one of the agents can't make another move.
+      // The game is over if one of the agents can't make another move.
 
-    	return holesEmpty(board, Side.NORTH) || holesEmpty(board, Side.SOUTH);
+      return holesEmpty(board, Side.NORTH) || holesEmpty(board, Side.SOUTH);
     }
 }
 
--- MKAgent/Main.java
+++ MKAgent/Main.java
@@ -1,9 +1,14 @@
 package MKAgent;
+import java.awt.Component;
+import java.awt.Dimension;
 import java.io.BufferedReader;
 import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.util.Random;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 /**
  * The main application class. It also provides methods for communication
@@ -11,49 +16,96 @@ import java.io.Reader;
  */
 public class Main
 {
-    /**
-     * Input from the game engine.
-     */
-    private static Reader input = new BufferedReader(new InputStreamReader(System.in));
+  /**
+   * Input from the game engine.
+   */
+  private static Reader input = new BufferedReader(new InputStreamReader(System.in));
+  private static Component frame = new JFrame("TITLE");
 
-    /**
-     * Sends a message to the game engine.
-     * @param msg The message.
-     */
-    public static void sendMsg (String msg)
+  /**
+   * Sends a message to the game engine.
+   * @param msg The message.
+   */
+  public static void sendMsg (String msg)
+  {
+    System.out.print(msg);
+    System.out.flush();
+  }
+
+  /**
+   * Receives a message from the game engine. Messages are terminated by
+   * a '\n' character.
+   * @return The message.
+   * @throws IOException if there has been an I/O error.
+   */
+  public static String recvMsg() throws IOException
+  {
+    StringBuilder message = new StringBuilder();
+    int newCharacter;
+
+    do
     {
-    	System.out.print(msg);
-    	System.out.flush();
-    }
+      newCharacter = input.read();
+      if (newCharacter == -1)
+        throw new EOFException("Input ended unexpectedly.");
+      message.append((char)newCharacter);
+    } while((char)newCharacter != '\n');
+//    JOptionPane.showMessageDialog(frame, message.toString());
+  return message.toString();
+  }
 
-    /**
-     * Receives a message from the game engine. Messages are terminated by
-     * a '\n' character.
-     * @return The message.
-     * @throws IOException if there has been an I/O error.
-     */
-    public static String recvMsg() throws IOException
+  /**
+   * The main method, invoked when the program is started.
+   * @param args Command line arguments.
+   */
+  public static void main(String[] args) throws IOException, InvalidMessageException, Exception
+  {
+    frame.setSize(new Dimension(300, 400));
+    frame.setVisible(true);
+    // TODO: implement
+    String message = recvMsg();
+    MsgType startMsg = Protocol.getMessageType(message);
+    Side side = Side.SOUTH;
+    if (startMsg.equals(MsgType.START))
     {
-    	StringBuilder message = new StringBuilder();
-    	int newCharacter;
-
-    	do
-    	{
-    		newCharacter = input.read();
-    		if (newCharacter == -1)
-    			throw new EOFException("Input ended unexpectedly.");
-    		message.append((char)newCharacter);
-    	} while((char)newCharacter != '\n');
-
-		return message.toString();
+      side = Protocol.interpretStartMsg(message) ? Side.SOUTH : Side.NORTH;
     }
-
-	/**
-	 * The main method, invoked when the program is started.
-	 * @param args Command line arguments.
-	 */
-	public static void main(String[] args)
-	{
-		// TODO: implement
-	}
+    else throw new Exception();
+//    JOptionPane.showMessageDialog(frame,side);
+    Random random = new Random();
+    Board board = new Board(7, 7);
+    Kalah kalah = new Kalah(board);
+    int randomInt = -1;
+    while (true)
+    {
+      do
+      {
+        randomInt = (randomInt + 1) % 7;
+      } while (!kalah.isLegalMove(new Move(side, randomInt+1)));
+        
+      
+      sendMsg(Protocol.createMoveMsg(randomInt+1));
+      kalah.makeMove(new Move(side, randomInt+1));
+      String received = recvMsg();
+      if (Protocol.getMessageType(received).equals(MsgType.STATE))
+      {
+        Protocol.MoveTurn moveTurn = Protocol.interpretStateMsg(received, kalah.getBoard());
+        if (kalah.gameOver()) break;
+//        JOptionPane.showMessageDialog(frame, kalah.getBoard().toString());
+//        Side sideAfter = kalah.makeMove(new Move(side, moveTurn.move));
+      }
+//      JOptionPane.showMessageDialog(frame, received + "\n" + randomInt + "\n" + kalah.getBoard().toString());
+    }
+    
+//    sendMsg(Protocol.createMoveMsg(2));
+//    MsgType message = Protocol.getMessageType(test);
+//    if (message.equals(MsgType.START))
+//    {
+//      Protocol.interpretStartMsg(test);
+//    }
+  }
 }
+/*
+javac MKAgent/*.java &&java -jar ManKalah.jar "java -jar MKRefAgent.jar" "java MKAgent/Main"
+javac MKAgent/*.java &&java -jar ManKalah.jar "java MKAgent/Main" "java -jar MKRefAgent.jar"
+*/
