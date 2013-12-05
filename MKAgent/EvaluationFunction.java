5074ceee61d4034aae39da31c155c790dca79daa
Adam Choi <mbax9ac3@E-C07KILF901.cs.man.ac.uk>
2013-12-05 12:12:18 +0000

    Update files

--- MKAgent/EvaluationFunction.java
+++ MKAgent/EvaluationFunction.java
@@ -64,7 +64,7 @@ public class EvaluationFunction
   
   public boolean endsInWell(Side side, int hole)
   {
-  	return(board.getSeeds(side, hole) + hole == 8)? true: false;
+  	return(currentBoard.getSeeds(side, hole) + hole == currentBoard.getNoOfHoles()+1)? true: false;
   }
   
     // if boardLength+1 is returned it means the ending position is the scoring well
